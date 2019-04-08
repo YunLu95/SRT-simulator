8f45e181a57f6d14e82973b9268c4c217903880c
YunLu95 <lylpfe@icloud.com>
2019-04-08 15:09:31 -0500

    with functins updated.

--- Srt.java
+++ Srt.java
@@ -36,18 +36,22 @@ public class Srt {
 
         normB = normalize(binDividend);
         System.out.print("Normalized Dividend: "+normB+"\n");
-//        System.out.println("Normalized B " + normB.toString());
-//        negNormB = negativeOfB(normB);
-//        n = normB.length() - 2;
-//        maxLeftShifts = n+1;
-//        numLeftShifts = 0;
-//        adjustAQ();
-//        System.out.println("adjust AQ " + AQ.toString());
-//        shiftOverZeros();
-//        System.out.println("shiftOverZeros AQ " + AQ.toString());
-//        addB(negNormB);
-//        System.out.println("subtractB AQ " + AQ.toString());
+
+        negNormB = negativeOfB(normB);
+        n = normB.length() - 2;
+        maxLeftShifts = n+1;
+        numLeftShifts = 0;
+        this.dividend=binDividend;
+        this.divisor=binDivisor;
+        adjustAQ();
+        System.out.println("adjust AQ " + AQ.toString());
+        shiftOverZeros();
+        System.out.println("shiftOverZeros AQ " + AQ.toString());
+        addB(negNormB);
+        System.out.println("subtractB AQ " + AQ.toString());
+
         //loop
+        System.out.println("\n");
     }
 
 
@@ -58,91 +62,75 @@ public class Srt {
         for(i = 0;i < hexSB.length(); i ++) {
             switch (hexSB.charAt(i)) {
                 case '.':
-//                    System.out.print(".");
                     binSB.append(".");
                     break;
                 case '0':
-//                    System.out.print("0000");
                     binSB.append("0000");
                     break;
                 case '1':
-//                    System.out.print("0001");
                     binSB.append("0001");
                     break;
                 case '2':
-//                    System.out.print("0010");
                     binSB.append("0010");
                     break;
                 case '3':
-//                    System.out.print("0011");
                     binSB.append("0011");
                     break;
                 case '4':
-//                    System.out.print("0100");
                     binSB.append("0100");
                     break;
                 case '5':
-//                    System.out.print("0101");
                     binSB.append("0101");
                     break;
                 case '6':
-//                    System.out.print("0110");
                     binSB.append("0110");
                     break;
                 case '7':
-//                    System.out.print("0111");
                     binSB.append("0111");
                     break;
                 case '8':
-//                    System.out.print("1000");
                     binSB.append("1000");
                     break;
                 case '9':
-//                    System.out.print("1001");
                     binSB.append("1001");
                     break;
                 case 'A':
                 case 'a':
-//                    System.out.print("1010");
                     binSB.append("1010");
                     break;
                 case 'B':
                 case 'b':
-//                    System.out.print("1011");
                     binSB.append("1011");
                     break;
                 case 'C':
                 case 'c':
-//                    System.out.print("1100");
                     binSB.append("1100");
                     break;
                 case 'D':
                 case 'd':
-//                    System.out.print("1101");
                     binSB.append("1101");
                     break;
                 case 'E':
                 case 'e':
-//                    System.out.print("1110");
                     binSB.append("1110");
                     break;
                 case 'F':
                 case 'f':
-//                    System.out.print("1111");
                     binSB.append("1111");
                     break;
                 default:
                     System.out.print("\nInvalid hexadecimal digit " + hexSB.charAt(i));
             }
         }
+        binSB.insert(0,"0");
         return binSB;
     }
 
     StringBuilder normalize(StringBuilder norm) {
 //        StringBuilder norm = new StringBuilder(divisor);
         while (norm.length() >= 2) {
-            if (norm.charAt(1) == '0') {
-                norm.deleteCharAt(1);
+            if (norm.charAt(2) == '0') {
+                norm.deleteCharAt(2);
                 norm.append('0');
                 shift++;
             } else {
