eb527a76b7bc2f3729d7f6af1fac421cc3f68af0
thejaniehettiarachchi <thejaie630@gmail.coom>
2019-04-09 16:27:02 -0500

    loop completed

--- Main.java
+++ Main.java
@@ -5,17 +5,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Srt test1 = new Srt(".de", ".e");
-        Srt test2 = new Srt(".19", ".5");
-        Srt test3 = new Srt(".cd", ".a");
-        Srt test4 = new Srt(".156", ".101110(binary)");
-        Srt test5 = new Srt(".232", ".011111(binary)");
-        Srt test6 = new Srt(".1111", ".22");
-        Srt test7 = new Srt(".3333", ".11");
-        Srt test8 = new Srt(".5AC2", ".79");
-        Srt test9 = new Srt(".9CDE11", ".abc");
-        Srt test10 = new Srt(".672300", ".def");
-        Srt test11 = new Srt(".ABCDEF", ".987");
+        Srt example1 = new Srt(".0cc", ".011001(binary)");
+        Srt example2 = new Srt(".0010110101(binary)", ".01101(binary)");
+        
+//        Srt test1 = new Srt(".de", ".e");
+//        Srt test2 = new Srt(".19", ".5");
+//        Srt test3 = new Srt(".cd", ".a");
+//        Srt test4 = new Srt(".156", ".101110(binary)");
+//        Srt test5 = new Srt(".232", ".011111(binary)");
+//        Srt test6 = new Srt(".1111", ".22");
+//        Srt test7 = new Srt(".3333", ".11");
+//        Srt test8 = new Srt(".5AC2", ".79");
+//        Srt test9 = new Srt(".9CDE11", ".abc");
+//        Srt test10 = new Srt(".672300", ".def");
+//        Srt test11 = new Srt(".ABCDEF", ".987");
 
 
     }
